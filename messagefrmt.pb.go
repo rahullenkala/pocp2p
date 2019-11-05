c6a8d03c112acffc541af2b31bb3ffb98c4c7e86
rahullenkala <rahul.lenkala@gmail.com>
2019-11-05 12:25:29 +0530

    type of data is changed

--- messagefrmt.pb.go
+++ messagefrmt.pb.go
@@ -5,8 +5,9 @@ package main
 
 import (
 	fmt "fmt"
-	proto "github.com/golang/protobuf/proto"
 	math "math"
+
+	proto "github.com/golang/protobuf/proto"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -20,88 +21,88 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-type Cntxtmessage_MsgType int32
+type Kipcntxtmessage_MsgType int32
 
 const (
-	Cntxtmessage_QUERY    Cntxtmessage_MsgType = 0
-	Cntxtmessage_RESPONSE Cntxtmessage_MsgType = 1
+	Kipcntxtmessage_QUERY    Kipcntxtmessage_MsgType = 0
+	Kipcntxtmessage_RESPONSE Kipcntxtmessage_MsgType = 1
 )
 
-var Cntxtmessage_MsgType_name = map[int32]string{
+var Kipcntxtmessage_MsgType_name = map[int32]string{
 	0: "QUERY",
 	1: "RESPONSE",
 }
 
-var Cntxtmessage_MsgType_value = map[string]int32{
+var Kipcntxtmessage_MsgType_value = map[string]int32{
 	"QUERY":    0,
 	"RESPONSE": 1,
 }
 
-func (x Cntxtmessage_MsgType) String() string {
-	return proto.EnumName(Cntxtmessage_MsgType_name, int32(x))
+func (x Kipcntxtmessage_MsgType) String() string {
+	return proto.EnumName(Kipcntxtmessage_MsgType_name, int32(x))
 }
 
-func (Cntxtmessage_MsgType) EnumDescriptor() ([]byte, []int) {
+func (Kipcntxtmessage_MsgType) EnumDescriptor() ([]byte, []int) {
 	return fileDescriptor_53b2d96e84549a01, []int{0, 0}
 }
 
-type Cntxtmessage struct {
-	Type                 Cntxtmessage_MsgType `protobuf:"varint,1,opt,name=type,proto3,enum=main.Cntxtmessage_MsgType" json:"type,omitempty"`
-	Data                 map[string]string    `protobuf:"bytes,2,rep,name=data,proto3" json:"data,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
-	Sid                  string               `protobuf:"bytes,3,opt,name=sid,proto3" json:"sid,omitempty"`
-	Delimiter            string               `protobuf:"bytes,4,opt,name=Delimiter,json=delimiter,proto3" json:"Delimiter,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}             `json:"-"`
-	XXX_unrecognized     []byte               `json:"-"`
-	XXX_sizecache        int32                `json:"-"`
+type Kipcntxtmessage struct {
+	Type                 Kipcntxtmessage_MsgType `protobuf:"varint,1,opt,name=type,proto3,enum=main.Kipcntxtmessage_MsgType" json:"type,omitempty"`
+	Data                 string                  `protobuf:"bytes,2,opt,name=data,proto3" json:"data,omitempty"`
+	Sid                  string                  `protobuf:"bytes,3,opt,name=sid,proto3" json:"sid,omitempty"`
+	Delimiter            string                  `protobuf:"bytes,4,opt,name=Delimiter,json=delimiter,proto3" json:"Delimiter,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}                `json:"-"`
+	XXX_unrecognized     []byte                  `json:"-"`
+	XXX_sizecache        int32                   `json:"-"`
 }
 
-func (m *Cntxtmessage) Reset()         { *m = Cntxtmessage{} }
-func (m *Cntxtmessage) String() string { return proto.CompactTextString(m) }
-func (*Cntxtmessage) ProtoMessage()    {}
-func (*Cntxtmessage) Descriptor() ([]byte, []int) {
+func (m *Kipcntxtmessage) Reset()         { *m = Kipcntxtmessage{} }
+func (m *Kipcntxtmessage) String() string { return proto.CompactTextString(m) }
+func (*Kipcntxtmessage) ProtoMessage()    {}
+func (*Kipcntxtmessage) Descriptor() ([]byte, []int) {
 	return fileDescriptor_53b2d96e84549a01, []int{0}
 }
 
-func (m *Cntxtmessage) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Cntxtmessage.Unmarshal(m, b)
+func (m *Kipcntxtmessage) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Kipcntxtmessage.Unmarshal(m, b)
 }
-func (m *Cntxtmessage) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Cntxtmessage.Marshal(b, m, deterministic)
+func (m *Kipcntxtmessage) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Kipcntxtmessage.Marshal(b, m, deterministic)
 }
-func (m *Cntxtmessage) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Cntxtmessage.Merge(m, src)
+func (m *Kipcntxtmessage) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Kipcntxtmessage.Merge(m, src)
 }
-func (m *Cntxtmessage) XXX_Size() int {
-	return xxx_messageInfo_Cntxtmessage.Size(m)
+func (m *Kipcntxtmessage) XXX_Size() int {
+	return xxx_messageInfo_Kipcntxtmessage.Size(m)
 }
-func (m *Cntxtmessage) XXX_DiscardUnknown() {
-	xxx_messageInfo_Cntxtmessage.DiscardUnknown(m)
+func (m *Kipcntxtmessage) XXX_DiscardUnknown() {
+	xxx_messageInfo_Kipcntxtmessage.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Cntxtmessage proto.InternalMessageInfo
+var xxx_messageInfo_Kipcntxtmessage proto.InternalMessageInfo
 
-func (m *Cntxtmessage) GetType() Cntxtmessage_MsgType {
+func (m *Kipcntxtmessage) GetType() Kipcntxtmessage_MsgType {
 	if m != nil {
 		return m.Type
 	}
-	return Cntxtmessage_QUERY
+	return Kipcntxtmessage_QUERY
 }
 
-func (m *Cntxtmessage) GetData() map[string]string {
+func (m *Kipcntxtmessage) GetData() string {
 	if m != nil {
 		return m.Data
 	}
-	return nil
+	return ""
 }
 
-func (m *Cntxtmessage) GetSid() string {
+func (m *Kipcntxtmessage) GetSid() string {
 	if m != nil {
 		return m.Sid
 	}
 	return ""
 }
 
-func (m *Cntxtmessage) GetDelimiter() string {
+func (m *Kipcntxtmessage) GetDelimiter() string {
 	if m != nil {
 		return m.Delimiter
 	}
@@ -109,28 +110,24 @@ func (m *Cntxtmessage) GetDelimiter() string {
 }
 
 func init() {
-	proto.RegisterEnum("main.Cntxtmessage_MsgType", Cntxtmessage_MsgType_name, Cntxtmessage_MsgType_value)
-	proto.RegisterType((*Cntxtmessage)(nil), "main.cntxtmessage")
-	proto.RegisterMapType((map[string]string)(nil), "main.cntxtmessage.DataEntry")
+	proto.RegisterEnum("main.Kipcntxtmessage_MsgType", Kipcntxtmessage_MsgType_name, Kipcntxtmessage_MsgType_value)
+	proto.RegisterType((*Kipcntxtmessage)(nil), "main.kipcntxtmessage")
 }
 
 func init() { proto.RegisterFile("messagefrmt.proto", fileDescriptor_53b2d96e84549a01) }
 
 var fileDescriptor_53b2d96e84549a01 = []byte{
-	// 233 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0xbf, 0x4b, 0xc4, 0x40,
-	0x10, 0x85, 0xdd, 0x24, 0xa7, 0xee, 0x78, 0x48, 0x1c, 0x2c, 0x96, 0xe3, 0x8a, 0x90, 0x2a, 0xd5,
-	0x22, 0x67, 0xa1, 0x58, 0x5f, 0x4a, 0x7f, 0xed, 0x69, 0x61, 0xb9, 0x9a, 0xf1, 0x08, 0x5e, 0x72,
-	0x21, 0x19, 0xc5, 0xfd, 0xe3, 0x05, 0xd9, 0x35, 0x8a, 0xc5, 0x75, 0x6f, 0x3e, 0xbe, 0x19, 0x1e,
-	0x03, 0x27, 0x0d, 0x0d, 0x83, 0x5d, 0xd3, 0x6b, 0xdf, 0xb0, 0xee, 0xfa, 0x2d, 0x6f, 0x31, 0x69,
-	0x6c, 0xdd, 0xe6, 0x5f, 0x02, 0xa6, 0x2f, 0x2d, 0x7f, 0xf2, 0x28, 0xa0, 0x86, 0x84, 0x5d, 0x47,
-	0x4a, 0x64, 0xa2, 0x38, 0x5e, 0xcc, 0xb4, 0xb7, 0xf4, 0x7f, 0x43, 0x5f, 0x0f, 0xeb, 0x07, 0xd7,
-	0x91, 0x09, 0x1e, 0x9e, 0x41, 0x52, 0x59, 0xb6, 0x2a, 0xca, 0xe2, 0xe2, 0x68, 0x31, 0xdf, 0xe1,
-	0x2f, 0x2d, 0xdb, 0xb2, 0xe5, 0xde, 0x99, 0x60, 0x62, 0x0a, 0xf1, 0x50, 0x57, 0x2a, 0xce, 0x44,
-	0x21, 0x8d, 0x8f, 0x38, 0x07, 0xb9, 0xa4, 0x4d, 0xdd, 0xd4, 0x4c, 0xbd, 0x4a, 0x02, 0x97, 0xd5,
-	0x2f, 0x98, 0x5d, 0x80, 0xfc, 0x3b, 0xe1, 0x97, 0xdf, 0xc8, 0x85, 0x76, 0xd2, 0xf8, 0x88, 0xa7,
-	0x30, 0xf9, 0xb0, 0x9b, 0x77, 0x52, 0x51, 0x60, 0x3f, 0xc3, 0x55, 0x74, 0x29, 0xf2, 0x1c, 0x0e,
-	0xc6, 0xae, 0x28, 0x61, 0x72, 0xff, 0x58, 0x9a, 0xa7, 0x74, 0x0f, 0xa7, 0x70, 0x68, 0xca, 0xd5,
-	0xdd, 0xed, 0xcd, 0xaa, 0x4c, 0xc5, 0xf3, 0x7e, 0x78, 0xc6, 0xf9, 0x77, 0x00, 0x00, 0x00, 0xff,
-	0xff, 0x9b, 0x97, 0x2e, 0x72, 0x21, 0x01, 0x00, 0x00,
+	// 184 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xcc, 0x4d, 0x2d, 0x2e,
+	0x4e, 0x4c, 0x4f, 0x4d, 0x2b, 0xca, 0x2d, 0xd1, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0xc9,
+	0x4d, 0xcc, 0xcc, 0x53, 0x5a, 0xc3, 0xc8, 0xc5, 0x9f, 0x9d, 0x59, 0x90, 0x9c, 0x57, 0x52, 0x51,
+	0x02, 0x55, 0x23, 0x64, 0xc8, 0xc5, 0x52, 0x52, 0x59, 0x90, 0x2a, 0xc1, 0xa8, 0xc0, 0xa8, 0xc1,
+	0x67, 0x24, 0xab, 0x07, 0x52, 0xa8, 0x87, 0xa6, 0x48, 0xcf, 0xb7, 0x38, 0x3d, 0xa4, 0xb2, 0x20,
+	0x35, 0x08, 0xac, 0x54, 0x48, 0x88, 0x8b, 0x25, 0x25, 0xb1, 0x24, 0x51, 0x82, 0x49, 0x81, 0x51,
+	0x83, 0x33, 0x08, 0xcc, 0x16, 0x12, 0xe0, 0x62, 0x2e, 0xce, 0x4c, 0x91, 0x60, 0x06, 0x0b, 0x81,
+	0x98, 0x42, 0x32, 0x5c, 0x9c, 0x2e, 0xa9, 0x39, 0x99, 0xb9, 0x99, 0x25, 0xa9, 0x45, 0x12, 0x2c,
+	0x60, 0x71, 0xce, 0x14, 0x98, 0x80, 0x92, 0x12, 0x17, 0x3b, 0xd4, 0x50, 0x21, 0x4e, 0x2e, 0xd6,
+	0xc0, 0x50, 0xd7, 0xa0, 0x48, 0x01, 0x06, 0x21, 0x1e, 0x2e, 0x8e, 0x20, 0xd7, 0xe0, 0x00, 0x7f,
+	0xbf, 0x60, 0x57, 0x01, 0xc6, 0x24, 0x36, 0xb0, 0xdb, 0x8d, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff,
+	0x1d, 0xfb, 0x1d, 0x7b, 0xd0, 0x00, 0x00, 0x00,
 }
